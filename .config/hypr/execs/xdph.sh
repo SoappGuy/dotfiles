39430fb38b9570fad28a1c58e2e2076c76471dee
dumbnerd <107756166+SoappGuy@users.noreply.github.com>
2024-06-20 00:34:26 +0300

    hypr fixes

--- .config/hypr/execs/xdph.sh
+++ .config/hypr/execs/xdph.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 
-sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-gtk
 killall -e xdg-desktop-portal-gnome
 killall -e xdg-desktop-portal
+
 /usr/lib/xdg-desktop-portal-hyprland &
 hyprctl notify -1 2000 "rgb(00ffff)" "XDP-H"
-sleep 0.2
+sleep 0.1
 /usr/lib/xdg-desktop-portal-gtk &
 hyprctl notify -1 2000 "rgb(00ffff)" "XDP-GTK"
-sleep 0.2
+sleep 0.1
 /usr/lib/xdg-desktop-portal-gnome &
 hyprctl notify -1 2000 "rgb(00ffff)" "XDP-GNOME"
-sleep 0.2
+sleep 0.1
 /usr/lib/xdg-desktop-portal &
 hyprctl notify -1 1000 "rgb(00ffff)" "XDP"
 
