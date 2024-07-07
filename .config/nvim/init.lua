2f7a42b8dae3780f99d8e7e4f9fb7f1d14ba9359
Yehor Sytnyk <github@dev.cuniling.us>
2024-07-07 18:05:22 +0300

    todo for nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,5 +3,14 @@ require 'core.keymaps'
 require 'core.autocmd'
 require 'core.lazy'
 
+--[[ TODO: 
+--    another file tree
+--    debug marks colors
+--    rust
+--    harpoon
+--    plugins from nvchad
+--    typos cheks
+--]]
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
