ca1fab778874712e196ec6a2416e2cf224b9f5de
Yehor Sytnyk <github@dev.cuniling.us>
2024-08-17 23:47:02 +0300

    persistent folds

--- .config/nvim/lua/core/autocmd.lua
+++ .config/nvim/lua/core/autocmd.lua
@@ -22,3 +22,31 @@ vim.api.nvim_create_autocmd('TermClose', {
     vim.opt.spell = true
   end,
 })
+
+local view_group = vim.api.nvim_create_augroup('auto_view', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'BufWinLeave', 'BufWritePost', 'WinLeave' }, {
+  desc = 'Save view with mkview for real files',
+  group = view_group,
+  callback = function(args)
+    if vim.b[args.buf].view_activated then
+      vim.cmd.mkview { mods = { emsg_silent = true } }
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  desc = 'Try to load file view if available and enable view saving for real files',
+  group = view_group,
+  callback = function(args)
+    if not vim.b[args.buf].view_activated then
+      local filetype = vim.api.nvim_get_option_value('filetype', { buf = args.buf })
+      local buftype = vim.api.nvim_get_option_value('buftype', { buf = args.buf })
+      local ignore_filetypes = { 'gitcommit', 'gitrebase', 'svg', 'hgcommit' }
+      if buftype == '' and filetype and filetype ~= '' and not vim.tbl_contains(ignore_filetypes, filetype) then
+        vim.b[args.buf].view_activated = true
+        vim.cmd.loadview { mods = { emsg_silent = true } }
+      end
+    end
+  end,
+})
