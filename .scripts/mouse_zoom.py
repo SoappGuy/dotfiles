22d17a8664f58a8e90674eecacafaeaf458c799d
Yehor Sytnyk <github@dev.cuniling.us>
2024-08-17 23:46:13 +0300

    some stuff

--- .scripts/mouse_zoom.py
+++ .scripts/mouse_zoom.py
@@ -1,8 +1,8 @@
 import subprocess
 import sys
 
-with open("/home/dumbnerd/.scripts/temp/mouse_scale_factor", "r") as file:
-    state = float(file.read().strip())
+with open("/tmp/mouse_scale_factor", "w+") as file:
+    state = float(file.read().strip() or 1)
 
     args = sys.argv[1:]
     if len(args) > 0:
@@ -13,12 +13,19 @@ with open("/home/dumbnerd/.scripts/temp/mouse_scale_factor", "r") as file:
         elif args[0] == "reset":
             state = 1.0
 
-with open("/home/dumbnerd/.scripts/temp/mouse_scale_factor", "w") as file:
     file.write(str(state))
 
-result = subprocess.run(["hyprctl", "keyword", "misc:cursor_zoom_factor", str(state)],
-                        check=True, text=True, capture_output=True)
+result = subprocess.run(
+    ["hyprctl", "keyword", "cursor:zoom_factor", str(state)],
+    check=True,
+    text=True,
+    capture_output=True,
+)
 
 if result.stdout != "ok":
-    subprocess.run(["hyprctl",  "notify",  "-1",  "1000", "rgb(0ff00)",
-                    result.stdout], check=True, text=True, capture_output=True)
+    subprocess.run(
+        ["hyprctl", "notify", "-1", "1000", "rgb(0ff00)", result.stdout],
+        check=True,
+        text=True,
+        capture_output=True,
+    )
