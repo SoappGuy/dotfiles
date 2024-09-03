827a9e23f932e8ce8449a4c16da2606fec0ac906
Yehor Sytnyk <github@dev.cuniling.us>
2024-09-03 08:55:41 +0300

    nure schedule

---
+++ .scripts/schedule.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Set period bounds
+START_TIME=$(date +%s -d '-1hour -35minutes')
+END_TIME=$(date +%s -d '12hours')
+
+# Set format
+FORMAT='{start_time} - {end_time} | {lecture_type} | {subject.brief}'
+
+# Get schedule for period with no labels using specified format
+SCHEDULE=$(                             \
+    nure_cli schedule group 'пзпі-23-2' \
+    --no_labels                         \
+    --sep   "\r"                        \
+    --fmt   "$FORMAT"                   \
+    --start "$START_TIME"               \
+    --end   "$END_TIME"                 \
+)
+
+# Extract first lecture
+FIRST_LINE=$(echo -e "$SCHEDULE" | cut -d $'\r' -f 1)
+
+printf '{"text": "%s", "tooltip": "%s", "class": ""}\n' "$FIRST_LINE" "$SCHEDULE"
