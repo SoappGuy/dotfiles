957468dd1be0ad5c4bdc8798023766f584c91160
Yehor Sytnyk <github@dev.cuniling.us>
2024-10-07 10:31:36 +0300

    cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,9 +4,5 @@ require 'core.autocmd'
 require 'core.lazy'
 
 vim.notify 'Loaded'
---[[ TODO: 
---    look at nvchad
---]]
 
--- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
