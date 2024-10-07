43d8f55295a018cc0d9061f73fdd28d3817fc95c
Yehor Sytnyk <github@dev.cuniling.us>
2024-10-07 10:23:14 +0300

    proper complitions setup

--- .config/nvim/lua/core/lazy/plugins/completions.lua
+++ .config/nvim/lua/core/lazy/plugins/completions.lua
@@ -2,22 +2,15 @@ return { -- Autocompletion
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
     {
       'L3MON4D3/LuaSnip',
       build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
         if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
           return
         end
         return 'make install_jsregexp'
       end)(),
       dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
         {
           'rafamadriz/friendly-snippets',
           config = function()
@@ -27,11 +20,11 @@ return { -- Autocompletion
       },
     },
     'saadparwaiz1/cmp_luasnip',
+    'onsails/lspkind.nvim',
 
-    -- Adds other completion capabilities.
-    --  nvim-cmp does not ship with all sources by default. They are split
-    --  into multiple repos for maintenance purposes.
+    'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-nvim-lsp-signature-help',
     'hrsh7th/cmp-path',
     'f3fora/cmp-spell',
   },
@@ -39,6 +32,8 @@ return { -- Autocompletion
     -- See `:help cmp`
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
+    local lspkind = require 'lspkind'
+
     luasnip.config.setup {}
 
     cmp.setup {
@@ -49,10 +44,6 @@ return { -- Autocompletion
       },
       completion = { completeopt = 'menu,menuone,noinsert' },
 
-      -- For an understanding of why these mappings were
-      -- chosen, you will need to read `:help ins-completion`
-      --
-      -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert {
         -- Select the [n]ext item
         ['<C-n>'] = cmp.mapping.select_next_item(),
@@ -78,7 +69,7 @@ return { -- Autocompletion
         --  Generally you don't need this, because nvim-cmp will display
         --  completions whenever it has completion options available.
         ['<C-Space>'] = cmp.mapping.complete {},
-
+        --
         -- Think of <c-l> as moving to the right of your snippet expansion.
         --  So if you have a snippet that's like:
         --  function $name($args)
@@ -102,19 +93,44 @@ return { -- Autocompletion
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
       sources = {
+
         { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'buffer' },
         { name = 'luasnip' },
         { name = 'path' },
-        {
-          name = 'spell',
-          option = {
-            keep_all_entries = false,
-            enable_in_context = function()
-              return true
-            end,
-            preselect_correct_word = true,
-          },
+        -- {
+        --   name = 'spell',
+        --   option = {
+        --     keep_all_entries = false,
+        --     enable_in_context = function()
+        --       return true
+        --     end,
+        --     preselect_correct_word = true,
+        --   },
+        -- },
+      },
+
+      window = {
+        completion = cmp.config.window.bordered {
+          border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+          col_offset = -3,
+          side_padding = 0,
         },
+        documentation = cmp.config.window.bordered {
+          border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+        },
+      },
+      formatting = {
+        fields = { 'kind', 'abbr' },
+        format = function(entry, vim_item)
+          local kind = lspkind.cmp_format { mode = 'symbol', maxwidth = 50 }(entry, vim_item)
+
+          kind.kind = ' ' .. kind.kind .. ' '
+          kind.menu = ''
+
+          return kind
+        end,
       },
     }
   end,
