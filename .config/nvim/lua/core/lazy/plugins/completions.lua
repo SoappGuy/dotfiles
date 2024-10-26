5a569da403e55b844e6301ce40605f8740265186
Yehor Sytnyk <github@dev.cuniling.us>
2024-10-26 18:58:02 +0300

    dbee

--- .config/nvim/lua/core/lazy/plugins/completions.lua
+++ .config/nvim/lua/core/lazy/plugins/completions.lua
@@ -29,6 +29,15 @@ return { -- Autocompletion
     'f3fora/cmp-spell',
 
     {
+      'MattiasMTS/cmp-dbee',
+      dependencies = {
+        { 'kndndrj/nvim-dbee' },
+      },
+      ft = 'sql', -- optional but good to have
+      opts = {}, -- needed
+    },
+
+    {
       'Jezda1337/nvim-html-css',
       dependencies = {
         'nvim-treesitter/nvim-treesitter',
@@ -66,39 +75,17 @@ return { -- Autocompletion
       completion = { completeopt = 'menu,menuone,noinsert' },
 
       mapping = cmp.mapping.preset.insert {
-        -- Select the [n]ext item
-        ['<C-n>'] = cmp.mapping.select_next_item(),
-        -- Select the [p]revious item
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        -- ['<C-n>'] = cmp.mapping.select_next_item(),
+        -- ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
 
-        -- Scroll the documentation window [b]ack / [f]orward
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
 
-        -- Accept ([y]es) the completion.
-        --  This will auto-import if your LSP supports it.
-        --  This will expand snippets if the LSP sent a snippet.
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
-
-        -- If you prefer more traditional completion keymaps,
-        -- you can uncomment the following lines
-        --['<CR>'] = cmp.mapping.confirm { select = true },
-        --['<Tab>'] = cmp.mapping.select_next_item(),
-        --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-        -- Manually trigger a completion from nvim-cmp.
-        --  Generally you don't need this, because nvim-cmp will display
-        --  completions whenever it has completion options available.
-        ['<C-Space>'] = cmp.mapping.complete {},
-        --
-        -- Think of <c-l> as moving to the right of your snippet expansion.
-        --  So if you have a snippet that's like:
-        --  function $name($args)
-        --    $body
-        --  end
-        --
-        -- <c-l> will move you to the right of each of the expansion locations.
-        -- <c-h> is similar, except moving you backwards.
+        -- ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<C-Space>'] = cmp.mapping.confirm { select = true },
+        ['<C-S-Space>'] = cmp.mapping.complete {},
         ['<C-l>'] = cmp.mapping(function()
           if luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
@@ -109,9 +96,6 @@ return { -- Autocompletion
             luasnip.jump(-1)
           end
         end, { 'i', 's' }),
-
-        -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-        --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
       sources = {
 
@@ -120,6 +104,7 @@ return { -- Autocompletion
         { name = 'buffer' },
         { name = 'luasnip' },
         { name = 'html-css' },
+        { name = 'cmp-dbee' },
         { name = 'path' },
         -- {
         --   name = 'spell',
---
+++ .config/nvim/lua/core/lazy/plugins/db.lua
@@ -0,0 +1,20 @@
+return {
+  'kndndrj/nvim-dbee',
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+  },
+  build = function()
+    -- Install tries to automatically detect the install method.
+    -- if it fails, try calling it with one of these parameters:
+    --    "curl", "wget", "bitsadmin", "go"
+    require('dbee').install()
+  end,
+  config = function()
+    local dbee = require 'dbee'
+    dbee.setup(--[[optional config]])
+
+    vim.keymap.set('n', '<leader>td', function()
+      dbee.toggle()
+    end, { desc = 'Toggle DB helper' })
+  end,
+}
