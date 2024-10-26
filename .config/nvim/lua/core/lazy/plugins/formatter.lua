53b75ef2c115a06f497440fd4fbb6adf33c2154c
Yehor Sytnyk <github@dev.cuniling.us>
2024-10-26 18:58:15 +0300

    conform

--- .config/nvim/lua/core/lazy/plugins/formatter.lua
+++ .config/nvim/lua/core/lazy/plugins/formatter.lua
@@ -1,26 +1,33 @@
 return { -- Autoformat
   'stevearc/conform.nvim',
   lazy = false,
-  opts = {
-    notify_on_error = false,
-    format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
-      local disable_filetypes = { c = false, cpp = false }
-      return {
-        timeout_ms = 500,
-        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-      }
-    end,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      python = { 'isort', 'black' },
-      -- rust = { 'rustfmt' },
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      -- javascript = { { "prettierd", "prettier" } },
-    },
-  },
+  config = function()
+    local conform = require 'conform'
+    conform.setup {
+      notify_on_error = true,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = false, cpp = false }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        python = { 'isort', 'black' },
+        sql = { 'sql_formatter' },
+        -- rust = { 'rustfmt' },
+      },
+    }
+
+    conform.formatters.sql_formatter = {
+      prepend_args = {
+        '-c',
+        '{ "language": "mysql", "tabWidth": 4, "keywordCase": "upper", "linesBetweenQueries": 2}',
+      },
+    }
+  end,
 }
